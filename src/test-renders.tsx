d6d480e0f95cf5e63a9a08e96df8c9ae8deceece
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2023-01-04 14:24:34 -0300

    chore(tests): add custom render with providers

---
+++ src/test-renders.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react'
+import { render, RenderOptions } from '@testing-library/react'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './lib/store'
+
+const Providers = ({ children }: React.PropsWithChildren<unknown>) => {
+  return (
+    <BrowserRouter>
+      <Provider store={store}>{children}</Provider>
+    </BrowserRouter>
+  )
+}
+
+const renderer = (
+  ui: React.ReactElement,
+  options?: Omit<RenderOptions, 'wrapper'>
+) => render(ui, { wrapper: Providers, ...options })
+
+export * from '@testing-library/react'
+export { renderer as render }
