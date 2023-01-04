e51422fc8005da4d6c5865fbe0c0a3f6c73753be
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2023-01-04 04:43:13 -0300

    fix(main): fix static height

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -2,9 +2,10 @@ import styled from 'styled-components'
 
 export const Main = styled.main`
   width: 100%;
-  height: 100vh;
+  min-height: 100vh;
   padding: 1rem;
   display: grid;
   place-items: center;
+  padding: 32px 0;
   background-color: #f7fafc;
 `
