ce5df07b299737660d68ac38c6137e66d1fcd665
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2023-01-04 16:12:24 -0300

    feat(component): add empty state

---
+++ src/components/empty-state.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const EmptyState = styled.div`
+  width: 100%;
+  display: grid;
+  place-items: center;
+
+  font-size: 1rem;
+  color: #718096;
+`
