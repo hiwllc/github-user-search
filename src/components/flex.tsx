e6f87037f29215425994a2da61990bfb09b6ec42
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2023-01-05 14:42:43 -0300

    refactor(components): add props to flex component

--- src/components/flex.tsx
+++ src/components/flex.tsx
@@ -1,11 +1,21 @@
 import styled from 'styled-components'
 
-export const Flex = styled.div`
+type Props = {
+  justifyContent?:
+    | 'center'
+    | 'flex-start'
+    | 'flex-end'
+    | 'space-between'
+    | 'space-evenly'
+}
+
+export const Flex = styled.div<Props>`
   gap: 0.5rem;
   display: flex;
   align-items: center;
   flex-direction: row;
   flex-wrap: wrap;
+  justify-content: ${props => props.justifyContent ?? 'flex-start'};
 
   @media (min-width: 768px) {
     gap: 1rem;
